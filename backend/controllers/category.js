26c744830d04992b7894cf4fc4cf2c10e5efd965
majocasola <mjcasola@gmail.com>
2022-11-11 14:48:33 -0300

    create category save in mongodb

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -1,6 +1,20 @@
+import Category from '../models/category.js';
+import slugify from 'slugify';
+
+
 export const create = async(req, res) => {
     try {
-        console.log(req.body)
+        const { name } = req.body;
+        if (!name.trim()) {
+            return res.json({ error: 'El nombre es obligatorio' });
+        }
+        const existingCategory = await Category.findOne({ name });
+        if (existingCategory) {
+            return res.json({ error: 'La categoría ya existe' });
+        }
+
+        const category = await new Category({ name, slug: slugify(name) }).save();
+        res.json(category);
     } catch (err) {
         console.log(err);
         return res.status(400).json(err);
