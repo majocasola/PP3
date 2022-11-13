b6b6176ce6df6569138d555df08f75ad8326da51
majocasola <mjcasola@gmail.com>
2022-11-13 15:26:08 -0300

    products part 9

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -4,8 +4,7 @@ import slugify from "slugify";
 
 export const create = async(req, res) => {
     try {
-        // console.log(req.fields);
-        // console.log(req.files);
+
         const { name, description, price, category, quantity, shipping } = req.fields;
         const { photo } = req.files;
 
@@ -89,5 +88,50 @@ export const remove = async(req, res) => {
         res.json(product);
     } catch (err) {
         console.log(err);
+    }
+};
+
+
+export const update = async(req, res) => {
+    try {
+
+        const { name, description, price, category, quantity, shipping } = req.fields;
+        const { photo } = req.files;
+
+        // validation
+        switch (true) {
+            case !name.trim():
+                res.json({ error: "El nombre es requerido" });
+            case !description.trim():
+                res.json({ error: "La descripción es requerida" });
+            case !price.trim():
+                res.json({ error: "El precio es requerido" });
+            case !category.trim():
+                res.json({ error: "La categoría es requerida" });
+            case !quantity.trim():
+                res.json({ error: "La cantidad es requerida" });
+            case !shipping.trim():
+                res.json({ error: "El envío es requerido" });
+            case photo && photo.size > 1000000:
+                res.json({ error: "La imagen debe ser menor a 1MB" });
+        }
+
+        // update product
+        const product = await Product.findByIdAndUpdate(req.params.productId, {
+            ...req.fields,
+            slug: slugify(name),
+        }, { new: true });
+
+        if (photo) {
+            product.photo.data = fs.readFileSync(photo.path);
+            product.photo.contentType = photo.type;
+        }
+
+        await product.save();
+        res.json(product);
+
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json(err.message);
     }
 };
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,13 +6,14 @@ const router = express.Router();
 // middlewares
 import { requireSignin, isAdmin } from '../middlewares/auth.js';
 // controllers
-import { create, list, read, photo, remove } from '../controllers/product.js';
+import { create, list, read, photo, remove, update } from '../controllers/product.js';
 
 router.post('/product', requireSignin, isAdmin, formidable(), create);
 router.get('/products', list);
 router.get('/product/:slug', read);
 router.get('/product/photo/:productId', photo);
 router.delete('/product/:productId', requireSignin, isAdmin, remove);
+router.put('/product/:productId', requireSignin, isAdmin, formidable(), update);
 
 
 export default router;
